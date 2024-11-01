5e58a5bf26e9c42d44e8ef397e8f0fd0a4e21d36
lucas <lucaston2323@gmail.com>
2024-11-01 16:44:39 -0300

    feat: integrando lógica para exibição da conversão e interação do usuário

--- src/com/conversor/ConversorApplication.java
+++ src/com/conversor/ConversorApplication.java
@@ -1,5 +1,6 @@
 package com.conversor;
 
+import com.conversor.models.CurrencyConverter;
 import com.conversor.services.ExchangeRateClient;
 
 import java.util.Scanner;
@@ -9,12 +10,29 @@ public class ConversorApplication {
         Scanner sc = new Scanner(System.in);
         ExchangeRateClient exchangeRateClient = new ExchangeRateClient();
 
-        try {
-            String resultado = String.valueOf(exchangeRateClient.exchangeRequisicao("BRL"));
-            System.out.println("Resultado da API: " + resultado);
-        } catch (Exception e) {
-            System.err.println("Erro ao chamar a API: " + e.getMessage());
-        }
+        int opcao1, opcao2;
+
+        do {
+            opcao1 = CurrencyConverter.exibirMenuMoedas(sc, "Converter de:");
+            if (opcao1 == 0) {
+                System.out.println("Saindo. . .");
+                break;
+            }
+
+            opcao2 = CurrencyConverter.exibirMenuMoedas(sc, "Para:");
+            if (opcao2 == 0) {
+                System.out.println("Saindo. . .");
+                break;
+            }
+
+            double valor = CurrencyConverter.solicitarValor(sc);
+
+            String escolha1 = CurrencyConverter.codigoMoeda(opcao1);
+            String escolha2 = CurrencyConverter.codigoMoeda(opcao2);
+
+            CurrencyConverter.processarConversao(exchangeRateClient, escolha1, escolha2, valor);
+
+        } while (true);
 
         sc.close();
     }
